3943d62dbc23c27d5903998ce2221052a1b1a10f
Andur Carr <carr.andur@gmail.com>
2014-10-23 21:23:48 -0700

    Implement devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024022458) do
+ActiveRecord::Schema.define(version: 20141024040834) do
 
   create_table "users", force: true do |t|
-    t.string   "username",   null: false
-    t.string   "email",      null: false
+    t.string   "username",                            null: false
+    t.string   "email",                               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
