884f2496212ee5120a7ae2667b61d7b4d61bb2c2
niconan <nick.hax@gmail.com>
2015-06-28 00:09:35 -0700

    working user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
+        sign_in @user
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
