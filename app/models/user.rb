d0194fa604931c9586bcd2fe84a8309c866865e8
niconan <nick.hax@gmail.com>
2015-06-28 00:21:42 -0700

    first and last names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, :email, :username, presence: true
+  validates :first_name, :last_name, length: { in: 3..24 }
 end
