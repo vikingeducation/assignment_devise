697d9ebc03dc21cc89aedf3123b5e3ccc9fdb2cc
David <davidmeza@outlook.com>
2015-08-04 12:03:16 -0400

    adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  # devise_for :users
+  root to: 'users#index'
   resources :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
