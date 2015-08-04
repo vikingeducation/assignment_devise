248bcdf25fabcbb110b20eb6ce22cf0747f76f94
Alice Fung <alice.o.fung@gmail.com>
2015-08-04 12:23:16 -0400

    vaildations workgit add .

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804154933) do
+ActiveRecord::Schema.define(version: 20150804160430) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",                            null: false
+    t.string   "username"
     t.string   "email",                               null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
