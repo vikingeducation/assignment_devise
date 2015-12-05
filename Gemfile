cb4bd1822faf010ecf59b9f9278526d16de040e3
Justin Mullis <justin.mullis@gmail.com>
2015-12-05 11:46:29 -0500

    assignment complete

--- Gemfile
+++ Gemfile
@@ -34,7 +34,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+  gem 'hirb'
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205154253) do
+ActiveRecord::Schema.define(version: 20151205163009) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email",              default: "", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.string   "first_name",         default: "", null: false
+    t.string   "last_name",          default: "", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
