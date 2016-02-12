8ffd2ea5b0b702f5991c5693e89ea7058e0834be
Sam O'Keefe <samok13@gmail.com>
2016-02-12 09:30:38 -0800

    finished devise practice

--- db/migrate/20160212162250_create_users.rb
+++ db/migrate/20160212162250_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, :null => false
+      t.string :username, :null => true
       t.string :email, :null => false
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps null: false
     end
