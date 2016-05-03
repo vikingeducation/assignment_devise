4b2b815013330e9f240016e5b2abd23d9df902b1
Steven Chang <prime_pork@hotmail.com>
2016-05-03 18:31:22 +1000

    Added first name and last name and devise and all that but not 100% sure about it

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
+gem 'devise'
+
+gem 'better_errors'
+gem 'binding_of_caller'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  devise_for :users
   resources :users
+
+  devise_scope :user do
+    get 'sign_in',  to: "devise/sessions#new"
+    get 'login',    to: "devise/sessions#new"
+    get 'sign_out', to: "devise/sessions#destroy"
+    get 'logout',   to: "devise/sessions#destroy"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
