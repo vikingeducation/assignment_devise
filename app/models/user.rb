946990da0ee71a30ceecceef12ce15a9763caadd
JeremyVe <jepschx@gmail.com>
2016-06-08 10:16:57 +0800

    added user model and devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable, 
+  # :recoverable, :rememberable, :trackable
+
+  devise :database_authenticatable, :registerable, :validatable
+
+  validates :username, :first_name, :last_name, presence: true
+
+  validates :first_name, :last_name, length: {minimum: 3}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607082341) do
+ActiveRecord::Schema.define(version: 20160608015020) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   null: false
-    t.string   "email",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username",                        null: false
+    t.string   "email",                           null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.string   "first_name",         default: "", null: false
+    t.string   "last_name",          default: "", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
