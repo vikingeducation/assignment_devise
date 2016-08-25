556db5772b02b62dc62c5f456dd9a33d9f5e3824
schoetlr <srlrs20020@gmail.com>
2016-08-25 10:25:00 -0400

    custom attributes for user devise model and authorization for protect actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_filter :authenticate_user!, only: [:index, :show]
 
   # GET /users
   # GET /users.json
