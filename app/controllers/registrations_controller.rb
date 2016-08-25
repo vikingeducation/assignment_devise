21f1e6bb44b1c5bcc7823c75b77b8e347366a7da
Bideo Wego <bideowego@gmail.com>
2016-08-25 17:44:31 -0400

    Update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,24 +2,25 @@ class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
     params.require(:user).permit(
-      :first_name, 
-      :last_name, 
+      :first_name,
+      :last_name,
       :username,
-      :email, 
-      :password, 
+      :email,
+      :password,
       :password_confirmation
     )
   end
 
   def account_update_params
     params.require(:user).permit(
-      :first_name, 
+      :first_name,
       :last_name,
-      :username, 
-      :email, 
-      :password, 
-      :password_confirmation, 
+      :username,
+      :email,
+      :password,
+      :password_confirmation,
       :current_password
     )
   end
 end
+
