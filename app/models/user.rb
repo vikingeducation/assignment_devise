1425626a5dcbf215341a0d6ea2c68607a190d03a
James Harris <computersherpa@gmail.com>
2016-12-08 11:07:43 -0600

    one new migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  devise :registerable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable, :database_authenticatable
 end
