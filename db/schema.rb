5e6bf8cad0cd5a73e52eeff67a4749f95b90ffa1
James Harris <computersherpa@gmail.com>
2016-12-08 10:54:17 -0600

    Started setting up Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208163526) do
+ActiveRecord::Schema.define(version: 20161208165029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   null: false
-    t.string   "email",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username",           null: false
+    t.string   "email",              null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "encrypted_password"
   end
 
 end
