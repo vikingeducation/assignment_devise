f7992a77e8008bd34257baae4ac5571538f4e31f
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-08 12:40:34 -0500

    Implement user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  before_filter :configure_sign_up_params, if: :devise_controller?
+
+  protected
+
+     def configure_sign_up_params
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'users#index'
 
+  # why singular ???
+  devise_for :user
+
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
