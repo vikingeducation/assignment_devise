4092f518dd6ab737941c26249b0cfa6eeb19789f
Roy Chen <roychenlei@gmail.com>
2017-09-28 14:00:31 +0800

    added User authentication and authorization with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :recoverable, :rememberable, :trackable, :validatable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable
+
   validates :username,
             :email,
             presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928052034) do
+ActiveRecord::Schema.define(version: 20170928052633) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
