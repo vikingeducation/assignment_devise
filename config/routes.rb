e2de5052ff4d4b4c82a15a1104e9dbe6af5d9708
Roy Chen <roychenlei@gmail.com>
2017-09-28 14:26:25 +0800

    setup custom routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
   resources :users
 
   root to: "users#index"
