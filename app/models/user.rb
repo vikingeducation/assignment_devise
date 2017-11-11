082c183c6a25b6e4ad499c235a02481d615840aa
andie5 <andieasa@googlemail.com>
2017-11-11 00:03:27 +0000

    installed devise

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # , :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
+
+  # Default devise modules
+  # devise :database_authenticable, :registrable, :rememberable
+
+  validates :username, :presence => true
+  validates :email, :presence => true
+
+
+  attr_accessor :username, :email, :password, :password_confirmation
+end
