4b39abced162c930d93a37233d4f6377dbe30da8
Chad Lucas <chad.lucas.19@gmail.com>
2017-11-11 17:34:21 -0800

    add first and last name to user table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+ 
+  protect_from_forgery prepend: true
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  #before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_users!, :except => [:new, :create, :index] 
   # GET /users
   # GET /users.json
   def index
